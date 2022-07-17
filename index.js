3e8dfa8f05845f1888223b40dbb1d0917b2777f0
wjuma19dev <wjuma19dev@yahoo.com>
2022-07-17 15:08:33 -0400

    Trabajando con las rutas principales, creado el directorio de routes junto con los archivos de admin.js y shop.js

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
+app.use('/add-product', (req, res, next) => {
+	res.send('<form method="POST" action="/product"><label>Producto</label><input name="producto" type="text"/><button>Agregar</button></form>');
+});
+
+app.post('/product', (req, res) => {
+	console.log(req.body);
+	res.redirect('/');
+})
+
+app.use((req, res, next) => {
+	console.log("I'am another middleware!");
+	res.send("Hello World!");
+});
 
 app.listen(
 	3000,
